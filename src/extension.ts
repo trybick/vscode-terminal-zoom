86c30d7813d2e86ed2a05550887b20472ddebd74
trybick <t.rybicki@gmail.com>
2019-11-29 09:41:44 -0500

    Refactor with setZoomLevel handling both cases

--- src/extension.ts
+++ src/extension.ts
@@ -3,17 +3,26 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
   const terminalFontSize = 'terminal.integrated.fontSize';
 
-  async function increaseFontSize() {
+  function getCurrentSize() {
     const config = vscode.workspace.getConfiguration();
-    const currentSize = config.get<number>(terminalFontSize) || 12;
-    config.update(terminalFontSize, currentSize + 2, true);
+    return config.get<number>(terminalFontSize) || 12;
+  }
+
+  async function increaseFontSize() {
+    setZoomLevel(getCurrentSize() + 1);
   }
 
   async function decreaseFontSize() {
-    const config = vscode.workspace.getConfiguration();
-    const currentSize = config.get<number>(terminalFontSize) || 12;
-    config.update(terminalFontSize, currentSize - 2, true);
+    setZoomLevel(getCurrentSize() - 1);
   }
+
+  const setZoomLevel = (newLevel: number): Thenable<void> =>
+    vscode.workspace.getConfiguration().update(terminalFontSize, newLevel, true);
+
+  const myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+  myStatusBarItem.text = 'Test';
+  context.subscriptions.push(myStatusBarItem);
+  myStatusBarItem.show();
 
   context.subscriptions.push(
     vscode.commands.registerCommand('fontshortcuts.decreaseTerminalFontSize', () =>
