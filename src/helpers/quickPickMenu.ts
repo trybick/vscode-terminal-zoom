59110d4405ba3fcaaa222ee2025e9f63afac3703
trybick <t.rybicki@gmail.com>
2019-11-30 15:24:37 -0500

    Add custom input to quick pick - breaking some behaviors

--- src/helpers/quickPickMenu.ts
+++ src/helpers/quickPickMenu.ts
@@ -1,3 +1,4 @@
+import { getCurrentSize } from './statusBar';
 import { QuickPickItem, window } from 'vscode';
 import { strings } from './constants';
 import { setFontSize } from '../extension';
@@ -6,10 +7,27 @@ export async function openQuickPick() {
   const placeHolder = strings.quickPickPlaceholder;
   const optionsRange = [...Array(25).keys()].filter(i => i >= 8 && i % 2 === 0); // even numbers from 8-24
   const options: QuickPickItem[] = optionsRange.map(num => {
-    return { label: `${num.toString()}-pt` };
+    return { label: `$(text-size) ${num.toString()}-pt` };
+    // FIXME ^^ the text-size is interferring with setting the number
+  });
+
+  options.push({
+    label: `$(pencil) Input font-size`
   });
 
   const userSelection = await window.showQuickPick(options, { placeHolder });
+
+  if (userSelection && userSelection.label === `$(pencil) Input font-size`) {
+    const enteredSize = await window.showInputBox({
+      prompt: 'Enter custom font-size',
+      value: String(getCurrentSize())
+    });
+    if (undefined !== enteredSize) {
+      setFontSize(Number(enteredSize));
+    }
+    return;
+  }
+
   const newFontSize = userSelection && userSelection.label.slice(0, -3);
 
   if (newFontSize) {
